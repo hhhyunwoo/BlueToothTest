72d2a5433300686263ef30dcd2178ba6c6ec7a0d
qrlagusdn <qrlagusdn@naver.com>
2018-07-24 15:42:44 +0900

    You can click the listView(paired devices) and if your phone can connect with the device it will sent "Hello Wordl' to the paired device.

--- AndroidFile/app/src/main/java/com/example/qgusd/fragmenttest/MainActivity.java
+++ AndroidFile/app/src/main/java/com/example/qgusd/fragmenttest/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.qgusd.fragmenttest;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,8 +15,11 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Set;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
     MainFragment mainFragment;
@@ -98,7 +102,7 @@ public class MainActivity extends AppCompatActivity {
 
                 // Get the element clicked and send Hello world to it
                 String listElement = (String)parent.getItemAtPosition(position);
-                //sayHelloToDevice(listElement);
+                sayHelloToDevice(listElement);
 
             }
         });
@@ -106,4 +110,57 @@ public class MainActivity extends AppCompatActivity {
     ///////////////////////paird devices 를 listView에 넣어서 표시
     ///////////////////////onItemClickListener를 사용하여서 sayHelloDevice메서드 호출하도록
 
+    private void sayHelloToDevice(String deviceName) {
+
+        UUID SPP_UUID = java.util.UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+        // Get the Bluetooth device with the given name
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        BluetoothDevice targetDevice = null;
+        for(BluetoothDevice pairedDevice : pairedDevices)
+            if(pairedDevice.getName().equals(deviceName)) {
+                targetDevice = pairedDevice;
+                break;
+            }
+
+        // If the device was not found, toast an error and return
+        if(targetDevice == null) {
+            Toast.makeText(this, "Device not found", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Create a connection to the device with the SPP UUID
+        BluetoothSocket btSocket = null;
+        try {
+            btSocket = targetDevice.createInsecureRfcommSocketToServiceRecord(SPP_UUID);
+        } catch (IOException e) {
+            Toast.makeText(this, "Unable to open a serial socket with the device", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Connect to the device
+        try {
+            btSocket.connect();
+        } catch (IOException e) {
+            Toast.makeText(this, "Unable to connect to the device", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        try {
+            OutputStreamWriter writer = new OutputStreamWriter(btSocket.getOutputStream());
+            writer.write("Hello world!\r\n");
+            writer.flush();
+        } catch (IOException e) {
+            Toast.makeText(this, "Unable to send message to the device", Toast.LENGTH_SHORT).show();
+        }
+
+        try {
+            btSocket.close();
+            Toast.makeText(this, "Message successfully sent to the device", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            Toast.makeText(this, "Unable to close the connection to the device", Toast.LENGTH_SHORT).show();
+        }
+    }
+    ///////////listView 누루면 그 디바이스로 Hello World String 전달
+    //////////////////////////////////////
 }
